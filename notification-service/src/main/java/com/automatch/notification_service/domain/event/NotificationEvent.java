043dfa2746a1d1f7ca1a3a5a56ec085383860928
Jose Carlos <joseka.vf@gmail.com>
2026-06-04 03:01:52 -0300

    feat(notification): add NotificationEvent domain DTO

---
+++ notification-service/src/main/java/com/automatch/notification_service/domain/event/NotificationEvent.java
@@ -0,0 +1,15 @@
+package com.automatch.notification_service.domain.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationEvent {
+    private String recipient;
+    private String message;
+}
