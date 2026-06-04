4c5dd9662fe760e25cba7dcef6ae3fd9c1dd4f7c
Jose Carlos <joseka.vf@gmail.com>
2026-06-04 02:57:05 -0300

    feat: add NotificationServiceApplication main class

---
+++ notification-service/src/main/java/com/automatch/notification_service/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.automatch.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
