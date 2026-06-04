b58d3b93e6e2331de9f68be3114a52b6a6cacccb
Jose Carlos <joseka.vf@gmail.com>
2026-06-04 03:08:43 -0300

    feat(booking): create NotificationEvent domain event

---
+++ booking-service/src/main/java/com/automatch/booking_service/domain/event/NotificationEvent.java
@@ -0,0 +1,15 @@
+package com.automatch.booking_service.domain.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationEvent {
+    private String email;
+    private String message;
+}
