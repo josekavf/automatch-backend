585f0ba5f41b33ca836a02d7c70c26f9d6a907dd
Jose Carlos <joseka.vf@gmail.com>
2026-06-04 03:08:56 -0300

    feat(booking): add publishNotification to EventPublisher interface

--- booking-service/src/main/java/com/automatch/booking_service/application/service/EventPublisher.java
+++ booking-service/src/main/java/com/automatch/booking_service/application/service/EventPublisher.java
@@ -1,7 +1,9 @@
 package com.automatch.booking_service.application.service;
 
 import com.automatch.booking_service.domain.event.BookingRequestedEvent;
+import com.automatch.booking_service.domain.event.NotificationEvent;
 
 public interface EventPublisher {
     void publishBookingRequested(BookingRequestedEvent event);
+    void publishNotification(NotificationEvent event);
 }
